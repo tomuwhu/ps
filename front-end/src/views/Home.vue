08a27e730e2d008959852428c6e1a0fe231584a7
Tamás Németh <tomuwhu@gmail.com>
2019-01-17 12:07:21 +0100

    .

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -14,7 +14,7 @@
              <a target='_blank' href='https://earth.google.com/web/@46.24909515,20.14647498,78.00118773a,72.55536563d,35y,105.7829675h,44.99598744t,-0r/data=ClEaTxJJCiUweDQ3NDQ4ODcwMzNmYzhlNzE6MHhiNmU2NTEyYWFiOGU1Y2Q0GTzkHCvnH0dAIQSGUbWCJTRAKg7DgXJww6FkIHTDqXIgMhgCIAE'>Iroda:</a> <a target='_blank' href='https://earth.google.com/web/@46.24918953,20.14634662,85.23022461a,0d,88.62942005y,94.56206024h,106.28188836t,0r/data=ClEaTxJJCiUweDQ3NDQ4ODcwMzNmYzhlNzE6MHhiNmU2NTEyYWFiOGU1Y2Q0GTzkHCvnH0dAIQSGUbWCJTRAKg7DgXJww6FkIHTDqXIgMhgBIAEiGgoWRkNKQmlidi1kd1JfZUlNd0ZUR0UzQRAC'>42-es oktatói szoba</a>
             </p>
             <p>Tel.: +36 (62) 34-3435</p>
-            <p>Ímél: tnemeth@'URL a /-ig'</p>
+            <p>Ímél: tnemeth@inf.u-szeg....</p>
         </div>
        </div>
     </div>
